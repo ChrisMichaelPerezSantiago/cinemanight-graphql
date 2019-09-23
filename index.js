330e70da10b0198842c84e4c00ea7785a8245e67
Chris Michael <chris.perez@upr.edu>
2019-09-22 22:04:07 -0400

    A single module has been defined as an entry point.

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const API = require('./src/api/API.js');
 
 const server = new ApolloServer({
   modules: [
-    require('./src/modules/episodes/index.js'),
-    require('./src/modules/series/index.js')
+    require('./src/modules/shows/index.js'),
   ],
   dataSources: () => ({
     API: new API()
